5e5558c3782bf0d40cc7eb20ffdae2a5ae488511
Ted Conway <tedconway@aol.com>
2014-05-24 23:31:08 -0500

    commit for peer review

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Coursera - R Programming - Programming Assignment 2
+# R functions to cache potentially time-consuming inverse matrix computations.
 
-## Write a short comment describing this function
+# Construct data structure from matrix passed by call. 
+# Usage example: z <- makeCacheMatrix(matrix(c(4, 2, 7, 6), 2))
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+# Return inverse of matrix passed in data structure passed. Inverse is returned from cache,
+# if available, or is computed using "solve" and saved in the cache for possible later use.
+# Usage example: cacheSolve(z)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
